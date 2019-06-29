09f7d008dc9466fe9a1c9b73825ad90bcb78a62a
Alec Hoyland <alec.hoyland@posteo.net>
2019-06-28 20:57:46 -0400

    save figures

--- code/generate_all_figures.m
+++ code/generate_all_figures.m
@@ -10,3 +10,5 @@
 % plot the dimensionally reduced data
 fig1 = figure('OuterPosition',[0 0 1600 1600],'PaperUnits','points','PaperSize',[1600 1600]);
 figure_dim_red_comparison(fig1, D);
+
+figlib.saveall
